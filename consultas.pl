bf98961680b1bbadae2f30e26e887cc0dd3257c7
isaparras23 <iparras@eafit.edu.co>
2025-09-29 12:36:58 -0500

    Update consultas.pl

--- consultas.pl
+++ consultas.pl
@@ -3,7 +3,7 @@
 generate_report(toyota, suv, 30000, R).
 
 % Caso 2:
-bagof(vehicle(ford, Ref, Tipo, Price, Year), vehicle(ford, Ref, Tipo, Price, Year), Lista).
+bagof(Reference-Price, vehicle(ford, Reference, Type, Price, Year), Vehiculos).
 
 % Caso 3:
 generate_report_max(_, sedan, 1000000, 500000, R).
